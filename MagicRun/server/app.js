66e881b16977afa491d090239455e19bb7fe5d62
Asen Mihailov <AsenJ.Mihaylov@gmail.com>
2018-04-27 16:51:46 +0300

    .

--- MagicRun/server/app.js
+++ MagicRun/server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const Player = require('./Controllers/Player');
+const Battle = require('./Controllers/Battle');
 
 app.use('/player', Player);
+app.use('/battle', Battle);
 
 app.listen(8000);
