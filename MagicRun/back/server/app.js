cd1ce4a8725d7c3c99a6256d698ffd194b697d3f
Asen Mihailov <AsenJ.Mihaylov@gmail.com>
2018-04-30 16:28:13 +0300

    Split front-end and back-end

--- MagicRun/back/server/app.js
+++ MagicRun/back/server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
+const Common = require('./Controllers/Common');
 const Player = require('./Controllers/Player');
 const Battle = require('./Controllers/Battle');
 
+app.use('/', Common);
 app.use('/player', Player);
 app.use('/battle', Battle);
 
